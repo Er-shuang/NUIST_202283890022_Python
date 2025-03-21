a90264571380fed57b2719f44ae9b103589dff76
Er-shuang <1343109389@qq.com>
2025-03-21 20:06:02 +0800

    Add MultiplyThreeNums.py from Raspberry Pi to repository

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,14 @@
+# Calculate the producr of three numbers
+# Author: Yujin Nie
+# Using *
+
+# Enter three numbers
+a = input("First number: ")
+b = input("Second number: ")
+c = input("Third number: ")
+
+# product of the three numbers
+product = int(a) * int(b) * int(c)
+
+# print the result
+print(product)
